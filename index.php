51a6e2230a06cd17854b45c159c7b6629736eb2b
Eduardo Bouças <mail@eduardoboucas.com>
2015-10-31 10:02:10 +0000

    Add stuff

--- index.php
+++ index.php
@@ -1,17 +1,38 @@
 <?php
+
+require 'vendor/autoload.php';
+
+/**
+ *
+ * Debug
+ *
+ */
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-// Load the XML source
-$xml = new DOMDocument;
-$xml->load('collection.xml');
 
-$xsl = new DOMDocument;
-$xsl->load('collection.xsl');
+/**
+ *
+ * Application
+ *
+ */
 
-// Configure the transformer
-$proc = new XSLTProcessor;
-$proc->importStyleSheet($xsl); // attach the xsl rules
+$app = new \Slim\Slim();
+$app->get('/test', function () {
+	// Load the XML source
+	$xml = new DOMDocument;
+	$xml->load('collection.xml');
 
-echo $proc->transformToXML($xml);
+	$xsl = new DOMDocument;
+	$xsl->load('collection.xsl');
+
+	// Configure the transformer
+	$proc = new XSLTProcessor;
+	$proc->importStyleSheet($xsl); // attach the xsl rules
+
+	echo $proc->transformToXML($xml);    
+});
+
+$app->run();
 
 ?>
